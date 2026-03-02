e5e44c1751f01ed6e146dc6650d244eac7dda424
sundaram <sundaramseth.ucer@gmail.com>
2026-03-03 04:28:29 +0530

    updates

--- hrms_backend/hrms_backend/settings.py
+++ hrms_backend/hrms_backend/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-CORS_ALLOW_ALL_ORIGINS = True  
+#CORS_ALLOW_ALL_ORIGINS = True  
 CORS_ALLOWED_ORIGINS = [
     "https://hrmslite-nine.vercel.app/",
 ]
